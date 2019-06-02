a97e7d7217724b04306604a337dc6d133ed0cba5
CY <you@example.com>
2019-06-01 19:49:30 -0500

    Updates

---
+++ precache-manifest.b01c653ee1aae0140e7a8565e048479f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e45ff145f55454cc7cd85d5599cc9d47",
+    "url": "/ReactTasks/index.html"
+  },
+  {
+    "revision": "801e67da86ea61884031",
+    "url": "/ReactTasks/static/css/main.1d4a9dd1.chunk.css"
+  },
+  {
+    "revision": "1dbc6b2763be16ba4094",
+    "url": "/ReactTasks/static/js/2.e148b302.chunk.js"
+  },
+  {
+    "revision": "801e67da86ea61884031",
+    "url": "/ReactTasks/static/js/main.edf1cf3c.chunk.js"
+  },
+  {
+    "revision": "2e6e5dcec4125cd55e7a",
+    "url": "/ReactTasks/static/js/runtime~main.d2fb6a01.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ReactTasks/static/media/logo.5d5d9eef.svg"
+  }
+]);
